c168772f867a22780307de12d6b94e5dd047fca0
itsbani <128512360+itsbani@users.noreply.github.com>
2023-03-28 09:09:28 -0400

    Animal Images added
    We have added various labels to show images of animals and an option of starting quiz along with the kind of animal are you.

---
+++ myQuizApp/myQuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myQuizApp
+//
+//  Created by Canadore Student on 2023-03-28.
+//  Copyright © 2023 canadore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
